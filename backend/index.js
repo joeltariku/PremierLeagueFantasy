2248826e90bcbea7762de4b38d82fb21597668db
joeltariku <jojotk10@gmail.com>
2025-07-12 00:52:33 -0400

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //app.use(cors());
 app.use(express.static('dist'))
--- client/src/services/getPlayers.ts
+++ client/src/services/getPlayers.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/api/topscorers"
+const baseUrl = "/api/topscorers"
 
 const getTopScorers = async (season: number, leagueId: number) => {
     const topScorers = 
