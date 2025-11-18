0acc9d2952d47b2595c8ba7007d3c40fb04c55a4
Joel <jojotk10@gmail.com>
2025-11-17 23:52:55 -0500

    added pipeline step to deploy code and added routes for health check and version

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,4 +18,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use('/api/leagues', leaguesRouter)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+    res.send('1')
+})
+
 export default app
