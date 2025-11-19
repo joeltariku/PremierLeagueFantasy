c45fbdc143ff00812fe9fd761082ec11069b1c84
Joel <jojotk10@gmail.com>
2025-11-18 23:12:26 -0500

    added endpoints to delete all leagues and delete league by id

--- backend/src/PremierLeague/controllers/leagues.ts
+++ backend/src/PremierLeague/controllers/leagues.ts
@@ -43,10 +43,37 @@ leaguesRouter.post('/', async (req: Request, res: Response) => {
 })
 
 leaguesRouter.delete('/', async (req: Request, res: Response) => {
-
+    try {
+        const result = await leaguesRepo.deleteAllLeagues()
+        res.json(`Deleted ${result} leagues`)
+    } catch (err) {
+        if (err && typeof err === 'object'  && 'detail' in err) {
+            res.status(500).json({ error: `Failed to delete leagues. ${err.detail}`})
+        } else if (err instanceof Error) {
+            res.status(500).json({ error: `Failed to delete leagues. ${err.message}`})
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred.'})
+        }
+    }
 })
 
 leaguesRouter.delete('/:id', async (req: Request, res: Response) => {
-    
+    try {
+        const leagueId = Number(req.params.id)
+        const result = await leaguesRepo.deleteLeagueById(leagueId)
+        if (result == 0) {
+            res.json(`No league exists with id=${leagueId}`)
+        } else {
+            res.json(`Deleted league with id=${leagueId}`)
+        }
+    } catch (err) {
+        if (err && typeof err === 'object' && 'detail' in err) {
+            res.status(500).json({ error: `Failed to delete league. ${err.detail}`})
+        } else if (err instanceof Error) {
+            res.status(500).json({ error: `Failed to delete league. ${err.message}`})
+        } else {
+            res.status(500).json({ error: 'An unknown error occurred.'})
+        }
+    }
 })
 export default leaguesRouter
